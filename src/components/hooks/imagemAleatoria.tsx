9315226534568ab8fa90d5fc57a01806d80994d4
Allan <allanangelonf@gmail.com>
2023-08-03 00:03:38 -0300

    second lesson image part 2

--- src/components/hooks/imagemAleatoria.tsx
+++ src/components/hooks/imagemAleatoria.tsx
@@ -1,16 +1,34 @@
-export default function ImagemAleatoria() {
+'use client'
+import Image from "next/image"
 
-  function renderizarBotao() {
+export default function ImagemAleatoria() {
+  let pesquisa: string = ''
+  const url = 'https://source.unsplash.com/featured/200x200?'
+
+  function urlImagem() {
+    return `${url}${pesquisa}`
+  }
+
+  function renderizarBotao(valor: string) {
     return (
-      <button>
-        Teste
+      <button className={`
+      bg-blue-500 px-4 py-2 rounded-md
+      `} onClick={() => {
+          pesquisa = valor
+        }}>
+        {valor}
       </button>
     )
   }
 
   return (
-    <div>
-      {renderizarBotao()}
+    <div className="flex flex-col gap-3 border border-zinc-500 p-7 rounded-md">
+      <Image src={urlImagem()} height={200} width={200} alt="Imagem" />
+      <div className="flex gap-5">
+        {renderizarBotao('abstract')}
+        {renderizarBotao('city')}
+        {renderizarBotao('person')}
+      </div>
     </div>
   )
 }
